8766e1c766bb7807f51be85386e2dc5bb14387f4
Yeondam Park <giyen9088@gmail.com>
2018-12-15 18:18:28 -0800

    user model added

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+//== Models
+const { User } = require("./models/user");
+
+//============
+//  USER ROUTE
+//============
+
+app.post("/api/users/register", (req, res) => {
+  res.status(200);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
 });
