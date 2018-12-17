934088b86a97ab0e62e1b8368ae7e624a201ab78
Yeondam Park <giyen9088@gmail.com>
2018-12-16 21:40:24 -0800

    hashing password

--- server/server.js
+++ server/server.js
@@ -24,11 +24,11 @@ const { User } = require("./models/user");
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
 
-  user.save((err, doc) => {
+  user.save((err, userData) => {
     if (err) return res.json({ success: false, err });
     res.status(200).json({
       success: true,
-      userData: doc
+      userData
     });
   });
 });
