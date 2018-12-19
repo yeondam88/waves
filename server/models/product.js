d8a6193ab2f3beef24c97639b35a204e7d8ca2d9
Yeondam Park <giyen9088@gmail.com>
2018-12-18 20:24:30 -0800

    query product by id

---
+++ server/models/product.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: 1,
+      maxlength: 100
+    },
+    description: {
+      type: String,
+      required: true,
+      maxlength: 1000
+    },
+    price: {
+      type: Number,
+      required: true,
+      maxlength: 255
+    },
+    brand: {
+      type: Schema.Types.ObjectId,
+      ref: "Brand",
+      required: true
+    },
+    shipping: {
+      type: Boolean,
+      required: true
+    },
+    available: {
+      type: Boolean,
+      required: true
+    },
+    wood: {
+      type: Schema.Types.ObjectId,
+      ref: "Wood",
+      required: true
+    },
+    frets: {
+      type: Number,
+      required: true
+    },
+    sold: {
+      type: Number,
+      maxlength: 100,
+      default: 0
+    },
+    publish: {
+      type: Boolean,
+      required: true
+    },
+    images: {
+      type: Array,
+      default: []
+    }
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = { Product };
--- server/server.js
+++ server/server.js
@@ -18,10 +18,45 @@ app.use(cookieParser());
 const { User } = require("./models/user");
 const { Brand } = require("./models/brand");
 const { Wood } = require("./models/wood");
+const { Product } = require("./models/product");
 
 //== Middleware
 const { auth } = require("./middleware/auth");
 const { admin } = require("./middleware/admin");
+
+//===================
+//      PRODUCTS
+//===================
+
+app.get("/api/product/product_by_id", (req, res) => {
+  let type = req.query.type;
+  let items = req.query.id;
+
+  if (type === "array") {
+    let ids = req.query.id.split(",");
+    items = [];
+    items = ids.map(item => {
+      return mongoose.Types.ObjectId(item);
+    });
+  }
+
+  Product.find({ _id: { $in: items } })
+    .populate("brand")
+    .populate("wood")
+    .exec((err, products) => {
+      return res.status(200).send(products);
+    });
+});
+
+app.post("/api/product/add", auth, admin, (req, res) => {
+  const product = new Product(req.body);
+
+  product.save((err, productData) => {
+    if (err) return res.json({ success: false, err });
+
+    res.status(200).json({ success: true, product: productData });
+  });
+});
 
 //===================
 //      WOODS
