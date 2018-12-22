fd946b726d9b576d79b5de1f45abf9c31b9fb443
Yeondam Park <giyen9088@gmail.com>
2018-12-21 22:22:51 -0800

    email validation with regexx

--- client/src/components/utils/Form/FormActions.js
+++ client/src/components/utils/Form/FormActions.js
@@ -1,6 +1,13 @@
 export const validate = (element, formData = []) => {
   let error = [true, ""];
 
+  if (element.validation.email) {
+    const valid = /\S+@\S+\.S+/.test(element.value);
+    const message = `${!valid ? "Must be a valid email." : ""}`;
+
+    error = !valid ? [valid, message] : error;
+  }
+
   if (element.validation.required) {
     const valid = element.value.trim() !== "";
     const message = `${!valid ? "This field is required." : ""}`;
